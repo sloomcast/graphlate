3dc87b6a95050bb1bfce5ad87eab322d6ffaeddf
Owen Davis <davisow@umich.edu>
2023-07-30 14:50:26 -0400

    very basic matrix structure and implementations

--- src/matrix/matrix.c
+++ src/matrix/matrix.c
@@ -1,3 +1,52 @@
+#include "matrix.h"
+
 //implementations of matrix.h
 
-#include "matrix.h"
+//REQUIRES: mat points to a valid matrix
+//          0 < width && width <= MAX_WIDTH
+//          0 < height && height <= MAX_HEIGHT
+//MODIFIES: *mat
+//EFFECTS: Initializes *mat as a matrix struct with the given width and height
+void matrix_init(struct matrix *mat, int width_init, int height_init){
+    //error handling here?
+
+    //initialization
+    mat->height = height_init;
+    mat->width = width_init;
+    mat->allocp = mat->data + width_init*height_init;
+}
+
+//REQUIRES: mat points to a valid matrix
+//EFFECTS: returns the width of the matrix pointed to by *mat
+int matrix_width(struct matrix *mat){
+    return mat->width;
+}
+
+//REQUIRES: mat points to a valid matrix
+//EFFECTS: returns the height of the matrix pointed to by *mat
+int matrix_height(struct matrix *mat){
+    return mat->height;
+}
+
+//REQUIRES: mat points to a valid matrix
+//          0 < width && width <= MAX_WIDTH
+//          0 < height && height <= MAX_HEIGHT
+//EFFECTS: returns a pointer to the value at row, col
+int* matrix_at(struct matrix *mat, int row, int col){
+    //error handling?
+
+    //finds the index in the 1d array
+    int index = row*matrix_width(mat) + col;
+    return &mat->data[index];
+}
+
+//REQUIRES: mat points to a valid matrix
+//MODIFIES: *mat
+//EFFECTS:  fills *mat with 0s as an empty matrix
+void matrix_fill_empty(struct matrix *mat){
+    for(int i=0; i<mat->height; i++){
+        for(int j=0; j<mat->width; j++){
+            *matrix_at(mat,i,j) = 0;
+        }
+    }
+}
--- test/Matrix_tests.c
+++ test/Matrix_tests.c
@@ -1,10 +1,19 @@
 //matrix tests to ensure compilation and smooth functioning
 
-#include "Matrix.h"
+#include "src/matrix/matrix.h"
 #include <stdio.h>
+#include <assert.h>
 
 int main(){
     printf("Hello!\n");
+    struct matrix mat1;
+    struct matrix *mp = &mat1;
+    matrix_init(mp,10,10);
+    matrix_fill_empty(mp);
+    assert(mp->data[0] == mp->data[1]);
+    assert(mp->data[0] == 0);
+    printf("Pass!\n");
+
 
     return 0;
 }
