37dd0ad338703ab8bd4659eca71b0c8b6349af91
Owen Davis <davisow@umich.edu>
2023-09-14 19:03:54 -0400

    code compiles WOOOOO, added implementations for everything into the .h file, no rand function yet, wrote test framework

--- newfiles/matrix.h
+++ newfiles/matrix.h
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include "list.h"
+#include <list>
 
 using namespace std;
 
@@ -13,28 +13,90 @@ const int MAX_DIMENSION = 10;
 template <typename T>
 class Matrix{
     public:
-    //Constructor
-    Matrix(int size);
+    Matrix(int size){
+        dimensions = size;
+    }
+
+    //default constructor
+    Matrix(){
+        dimensions = 10;
+        this->fill_random(3);
+    }
 
     //EFFECTS:  returns the width/height of the matrix
-    int matrix_dimension();
+    int matrix_dimension(){
+        return dimensions;
+    }
 
     //REQUIRES: 0 <= row,col <= dimensions
     //EFFECTS:  returns by reference the value stored at row,col
-    T* matrix_at(int row, int col);
+    T* matrix_at(int row, int col){
+        int index = row*dimensions + col;
+        return &data[index];
+    }
 
     //REQUIRES: 0 <= row <= dimensions
     //EFFECTS:  returns by reference the row at row
-    List<T> return_row(int row);
+    list<T> return_row(int row){
+        //make list
+        list<T> to_return;
+
+        //add to list
+        for(int i=0; i<dimensions; ++i){
+            to_return.push_front(matrix_at(row, i));
+        }
+
+        return to_return;
+    }
 
     //REQUIRES: 0 <= col <= dimensions
     //EFFECTS:  returns by reference the column at col
-    List<T> return_col(int col);
+    list<T> return_col(int col){
+        //make list
+        list<T> to_return;
+
+        //add to list
+        for(int i=0; i<dimensions; ++i){
+            to_return.push_front(matrix_at(i,col));
+        }
+
+        return to_return;
+    }
 
     //REQUIRES: os is a valid output stream capable of being written to
     //MODIFIES: the file specified in os
     //EFFECTS:  prints the matrix object to os
-    void print(ostream *os = nullptr);
+    void print(ofstream *os){
+        for(int i=0; i<dimensions; ++i){
+            for(int j=0; j<dimensions; ++j){
+                *os << matrix_at(i,j);
+            }
+        }
+    }
+
+    //REQUIRES: f_out is the name of an existing file or a file to be created
+    //MODIFIES: the file specified in f_out
+    //EFFECTS:  opens the file f_out or creates a new file and prints the matrix to it
+    void print(string f_out){
+        //creates stream
+        ofstream of;
+        of.open(f_out);
+
+        for(int i=0; i<dimensions; ++i){
+            for(int j=0; j<dimensions; ++j){
+                of << matrix_at(i,j);
+            }
+        }
+    }
+
+    //EFFECTS:  prints the matrix to stdout
+    void print(){
+        for(int i=0; i<dimensions; ++i){
+            for(int j=0; j<dimensions; ++j){
+                cout << matrix_at(i,j);
+            }
+        }
+    }
 
     //REQUIRES: option is and integer 1-4 corresponding to the desired fill type
     //MODIFIES: the matrix object the method is called on
@@ -43,7 +105,9 @@ class Matrix{
     //          2: irrational number
     //          3: integer
     //          4: natural number
-    void fill_random(int option);
+    void fill_random(int option){
+
+    }
 
     protected:
     int dimensions;
--- newfiles/matrix_tests.cpp
+++ newfiles/matrix_tests.cpp
@@ -2,7 +2,82 @@
 
 using namespace std;
 
+bool make_tests();
+bool at_tests();
+bool row_tests();
+bool col_tests();
+bool print_tests();
+bool rand_tests();
+
 int main(){
-    cout << "hello bitch";
+    cout << "hello bitch\n";
+
+    //make a matrix
+    if(make_tests()){
+        cout << "MAKING TESTS PASSED\n";
+    }
+    else cout << "MAKING TESTS FAILED YOU IDIOT\n";
+
+    //test matrix_at
+    if(at_tests()){
+        cout << "AT TESTS PASSED\n";
+    }
+    else cout << "AT TESTS FAILED YOU IDIOT\n";
+
+    //test return row
+    if(row_tests()){
+        cout << "ROW TESTS PASSED\n";
+    }
+    else cout << "ROW TESTS FAILED YOU IDIOT\n";
+
+    //test return column
+    if(col_tests()){
+        cout << "COL TESTS PASSED";
+    }
+    else cout << "COL TESTS FAILED YOU IDIOT\n";
+
+    //test print
+    if(print_tests()){
+        cout << "PRINT TESTS PASSED\n";
+    }
+    else cout << "PRINT TESTS FAILED YOU IDIOT\n";
+
+    //test random fill
+    if(rand_tests()){
+        cout << "RANDOM GENERATION TESTS PASSED\n";
+    }
+    else cout << "RANDOM GENERATION TESTS FAILED YOU IDIOT\n";
     return 0;
+}
+
+bool make_tests(){
+    Matrix<int> m1;
+    Matrix<int> m2(5);
+    Matrix<float> m3;
+    Matrix<string> lmfao;
+
+    if(m1.matrix_dimension() != 10) return false;
+    if(m2.matrix_dimension() != 5) return false;
+
+    return true;
+}
+
+bool at_tests(){
+    return true;
+}
+
+bool row_tests(){
+    return true;
+}
+
+bool col_tests(){
+    return true;
+}
+
+bool print_tests(){
+    return true;
+}
+
+bool rand_tests(){
+    return true;
 }
